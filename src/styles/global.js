9bb2f29faae90c0739694c326b0aba5582c58d36
Yusufvandy <yusufvandy@gmail.com>
2019-07-08 18:50:27 +0700

    create global styledcomponent and global css

---
+++ src/styles/global.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const SContainer = styled.div`
+max-width: 1140px;
+margin: auto;
+padding: 0 15px;
+`
+
+export const SCard = styled.div`
+    background-color: #fff;
+    padding: 25px;
+    border: 1px #ddd solid;
+    border-radius: 10px;
+    margin-top: 25px;
+`
+
+export const SActionButton = styled.button`
+    width: 100%;
+    background-image: linear-gradient(to right, #ff758c 0%, #ff7eb3 100%);
+    color: #fff;
+    border: none;
+    font-size: 14px;
+    padding: 15px;
+    font-weight: bold;
+    border-radius: 50px;
+    cursor: pointer;
+`
+
+export const SFormGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    text-align: left;
+    margin-bottom: 15px;
+`
