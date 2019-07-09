24199f58105e7e956d5f4b83060119dcf60f236c
Yusufvandy <yusufvandy@gmail.com>
2019-07-09 17:04:14 +0700

    create redux globalstore and auth reducers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navbar from './components/Navbar'
 import LoginCard from './components/LoginCard'
+import {connect} from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <LoginCard />
-    </div>
-  );
+class App extends Component {
+  render(){
+    console.log(this.props)
+    return (
+      <div className="App">
+        <Navbar />
+        <LoginCard />
+      </div>
+    );
+
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return{
+    username: state.auth.username
+  }
+}
+
+export default connect(mapStateToProps)(App);
+
