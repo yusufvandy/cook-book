795fc630c207bb1ce6194120517316b748d00398
Yusufvandy <yusufvandy@gmail.com>
2019-07-10 15:03:16 +0700

    react router test

--- src/container/Home.js
+++ src/container/Home.js
@@ -9,24 +9,41 @@ import {createUser} from '../actions/userAction'
 
 
 const Home = () => {
-    const menuList = [
-        {menu: 'Recipes', url: '/recipes'},
-        {menu: 'Sign In', url: '/signin'},
-        {menu: 'Sign Up', url: '/signup'},
-    ]
+  const menuList = [
+      {menu: 'Recipes', url: '/recipes'},
+      {menu: 'Sign In', url: '/signin'},
+      {menu: 'Sign Up', url: '/signup'},
+  ]
   const user = useSelector(state => state.auth.user);
   const dispatch = useDispatch()
 
   return (
     <Router>
-        <div>
-            <Navbar menus={menuList}/>
-            <LoginCard user={user} />
-            <button onClick={() => dispatch(createUser({username: 'test2', password: 'test2'}))}>SET</button>
-        </div>
+        <React.Fragment>
+          <Navbar menus={menuList}/>
+          <Switch>
+            <Route path='/' exact component={home}/>
+            <Route path='/recipes' component={recipes}/>
+            <Route path='/signup' component={signup}/>
+            {/* <LoginCard user={user} /> */}
+            {/* <button onClick={() => dispatch(createUser({username: 'test2', password: 'test2'}))}>SET</button> */}
+          </Switch>
+        </React.Fragment>
     </Router>
   );
 };
+
+const home = () => {
+  return<h1>This is HOME page</h1>
+}
+
+const recipes = () => {
+  return<h1>This is recipes page</h1>
+}
+
+const signup = () => {
+  return<h1>This is signup page</h1>
+}
 
 
 export default Home;
