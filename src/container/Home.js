08c7f41e9876b85d2e0998584e625d2d03c3d73f
Yusufvandy <yusufvandy@gmail.com>
2019-07-10 14:48:37 +0700

    create dynamic menu navbar

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import Navbar from "../components/Navbar";
 import LoginCard from "../components/LoginCard";
@@ -8,16 +9,24 @@ import {createUser} from '../actions/userAction'
 
 
 const Home = () => {
+    const menuList = [
+        {menu: 'Recipes', url: '/recipes'},
+        {menu: 'Sign In', url: '/signin'},
+        {menu: 'Sign Up', url: '/signup'},
+    ]
   const user = useSelector(state => state.auth.user);
   const dispatch = useDispatch()
 
   return (
-    <div>
-      <Navbar />
-      <LoginCard user={user} />
-      <button onClick={() => dispatch(createUser({username: 'test2', password: 'test2'}))}>SET</button>
-    </div>
+    <Router>
+        <div>
+            <Navbar menus={menuList}/>
+            <LoginCard user={user} />
+            <button onClick={() => dispatch(createUser({username: 'test2', password: 'test2'}))}>SET</button>
+        </div>
+    </Router>
   );
 };
+
 
 export default Home;
