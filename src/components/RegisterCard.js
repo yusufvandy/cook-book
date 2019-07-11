d97ccf489834e0ad8534ae37388ff2dc67f79180
Yusufvandy <yusufvandy@gmail.com>
2019-07-11 22:01:25 +0700

    set local state register component

--- src/components/RegisterCard.js
+++ src/components/RegisterCard.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import {useState, useEffect} from 'react'
+import { useSelector, useDispatch } from "react-redux";
+
 import { SContainer, SCard, SActionButton, SFormGroup } from '../styles/global'
+import {createUser} from '../actions/userAction'
 
 const STitle = styled.h1`
     font-size: 20px;
@@ -17,6 +21,24 @@ const SCardCustom = styled(SCard)`
 
 
 const RegisterCard = () => {
+    // const user = useSelector(state => state.auth.user);
+    const dispatch = useDispatch();
+
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        password: '',
+        retypePassword: ''
+    })
+    
+    const inputHandler = (e) => {
+        const {value, name} = e.target;
+        setUser({
+            ...user,
+            [name] : value
+        })
+    }
+    
     return (
         <React.Fragment>
             <SContainer>
@@ -27,19 +49,21 @@ const RegisterCard = () => {
                     <form action="">
                         <SFormGroup>
                             <label htmlFor="">Username</label>
-                            <input type="text"/>
+                            <input type="text" name="username" onChange={(e) => inputHandler(e)}/>
                         </SFormGroup>
                         <SFormGroup>
                             <label htmlFor="">Email</label>
-                            <input type="text"/>
+                            <input type="email" name="email" onChange={(e) => inputHandler(e)}/>
                         </SFormGroup>
                         <SFormGroup>
                             <label htmlFor="">New Password</label>
-                            <input type="password"/>
+                            <input type="password" name="password" onChange={(e) => inputHandler(e)}/>
                         </SFormGroup>
                         <SFormGroup>
                             <label htmlFor="">Retype Password</label>
-                            <input type="password"/>
+                            <input type="password" name="retypePassword" onChange={(e) => inputHandler(e)}/>
+                            {(user.password === user.retypePassword && user.password !== '') ? <label style={{marginTop: 5, color: 'green'}}>Password is correct</label> : 
+                            <label style={{marginTop: 5, color: 'red'}}>Wrong password, please retype password</label>}
                         </SFormGroup>
                         <SActionButton type="submit">Register</SActionButton>
                     </form>
--- src/container/Signup.js
+++ src/container/Signup.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { useSelector, useDispatch } from "react-redux";
 
 import RegisterCard from "../components/RegisterCard";
-import {createUser} from '../actions/userAction'
 
 
 
 const Signup = () => {
-    const user = useSelector(state => state.auth.user);
-    const dispatch = useDispatch()
-  
     return ( 
         <React.Fragment>
             <RegisterCard />
